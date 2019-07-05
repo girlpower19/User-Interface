0bc8fc8389d43e6c98c7f1024cfcb11a8f6cdc1a
prati-bha <pratibhahotwani.glsica15@gmail.com>
2019-07-05 12:21:36 +0530

    added functionality of navigation hiding at login and sign up.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route,Switch} from 'react-router-dom';
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
 import SignUpPage from '../SignUp';
@@ -11,25 +11,44 @@ import AccountPage from '../Account';
 import AdminPage from '../Admin';
 import * as ROUTES from '../../constants/routes';
 import withAuthentication from '../Session/withAuthentication';
-const loc=window.location.pathname;
-console.log(loc);
 const  App=()=>( 
   <div>
+    <Router>
+    <Switch>
+    <Route exact path={ROUTES.SIGN_UP} component={LoginContainer}/>
+    <Route exact path={ROUTES.SIGN_IN} component={LoginContainer}/>
+    <Route exact path={ROUTES.PASSWORD_FORGET} component={LoginContainer}/>
+    <Route exact path={ROUTES.PASSWORD_CHANGE} component={passwordChange} />
+    <Route component={DefaultContainer}/>
+    </Switch>
+    </Router>
+  </div>
+ );
+ const LoginContainer = () => (
+  <div>
+     <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
+    <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+    <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage}/>
+    <Route exact path={ROUTES.PASSWORD_CHANGE} component={passwordChange} />
 
-      <Router>
+  </div>
+)
+
+const DefaultContainer = () => (
+  <div>
+  
+ 
       <div>
-      {loc==='/signup' || loc==='/signin' ?<p></p>:<span><Navigation/><hr/></span>}
+     <Navigation/>
       <Route
       exact
       path={ROUTES.LANDING}
       component={LandingPage}/>
-          <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
-          <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
-          <Route
-            exact
-            path={ROUTES.PASSWORD_FORGET}
-            component={PasswordForgetPage}
-          />
+         <Route exact path={ROUTES.SIGN_UP} component={LoginContainer} />
+    <Route exact path={ROUTES.SIGN_IN} component={LoginContainer} />
+    <Route exact path={ROUTES.PASSWORD_FORGET} component={LoginContainer}/>
+    <Route exact path={ROUTES.PASSWORD_CHANGE} component={LoginContainer} />
+          
           <Route exact path={ROUTES.HOME} component={HomePage} />
           <Route
             exact
@@ -37,11 +56,10 @@ const  App=()=>(
             component={AccountPage}
           />
           <Route exact path={ROUTES.ADMIN} component={AdminPage} />
-          <Route exact path={ROUTES.PASSWORD_CHANGE} component={passwordChange} />
 
         </div>
-      </Router>
-      </div>
- );
-
+      
+  </div>
+  
+)
 export default withAuthentication(App);
