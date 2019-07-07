59a903d21dd085742cb6a16362747ac079ed0004
prati-bha <pratibhahotwani.glsica15@gmail.com>
2019-07-07 22:36:21 +0530

    Add Navbar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,37 +1,108 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { Link } from 'react-router-dom';
 import { AuthUserContext } from '../Session';
 import * as ROLES from '../../constants/roles';
-
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
+  MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon } from "mdbreact";
 
-const Navigation = () => (
-  <div style={{backgroundColor:'dodgerblue',fontSize:'150%',color:'white'}}>
+
+  const Navigation = () => (
   <AuthUserContext.Consumer>
   {
     authUser=>authUser ? <NavigationAuth authUser={authUser}/> : <NavigationNonAuth />}
    </AuthUserContext.Consumer>
-</div>
+
 
 );
 
-const NavigationAuth=({authUser})=> (
-  <div style={{display:'flex',justifyContent:'space-around'}}>
-      <Link style={{color:'white',textDecoration:'none',}}  to={ROUTES.LANDING}>Landing</Link>
-      <Link style={{color:'white',textDecoration:'none',}} to={ROUTES.HOME}>Home</Link>
-      <Link style={{color:'white',textDecoration:'none',}}   to={ROUTES.ACCOUNT}>Account</Link>
-{authUser.roles.includes(ROLES.ADMIN) &&(      <Link style={{color:'white',textDecoration:'none',}}   to={ROUTES.ADMIN}>Admin</Link>
-     )}
-      <SignOutButton />
-    </div>
-);
-const NavigationNonAuth = () => (
-  <div style={{display:'flex',justifyContent:'space-around',color:'white'}}>
-      <Link style={{color:'white',textDecoration:'none',}} to={ROUTES.LANDING}>Landing</Link>
-   
-      <Link style={{color:'white',textDecoration:'none',}} to={ROUTES.SIGN_IN}>Sign In</Link>
-    </div>
-);
+class NavigationAuth extends React.Component
+{
+  state = {
+    isOpen: false
+  };
+  
+  toggleCollapse = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+ 
+render()
+{
+  return(
+    <MDBNavbar color="light-blue" dark expand="md" className="blue-gradient">
+    <MDBNavbarToggler onClick={this.toggleCollapse} />
+    <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+    
+    <MDBNavbarBrand>
+    <MDBNavLink to={ROUTES.LANDING}>Girl Power</MDBNavLink>
+    </MDBNavbarBrand>
+    <MDBNavbarNav right>
+    <MDBNavItem>
+           {/* <MDBNavLink to={ROUTES.ACCOUNT}>Profile</MDBNavLink>*/}
+    </MDBNavItem>
+    <MDBNavItem>
+    {this.props.authUser.roles.includes(ROLES.ADMIN) &&(  <MDBNavLink to={ROUTES.ADMIN}>Admin</MDBNavLink>)}
+    </MDBNavItem>
+    
+    
+    <MDBNavItem>
+    <MDBDropdown dropleft>
+      <MDBDropdownToggle nav caret>
+        <MDBIcon icon="user" />
+      </MDBDropdownToggle>
+      <MDBDropdownMenu className="dropdown-default">
+        <MDBDropdownItem href={ROUTES.PASSWORD_CHANGE}> Change Password</MDBDropdownItem>
+        <MDBDropdownItem href="#"><SignOutButton/></MDBDropdownItem>
+      </MDBDropdownMenu>
+    </MDBDropdown>
+  </MDBNavItem>
+  </MDBNavbarNav>
+  </MDBCollapse>
+  </MDBNavbar>
+
+  )
+}
+}
+class NavigationNonAuth extends Component
+{
+  state = {
+    isOpen: false
+  };
+  
+  toggleCollapse = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+  render()
+  {
+    return(
+      <MDBNavbar color="light-blue" dark expand="md" className="blue-gradient">
+      <MDBNavbarToggler onClick={this.toggleCollapse} />
+      <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+      <MDBNavbarNav left>
+      <MDBNavbarBrand>
+      <MDBNavLink to={ROUTES.LANDING}>Girl Power</MDBNavLink>
+      </MDBNavbarBrand>
+      </MDBNavbarNav>
+      
+      <MDBNavbarNav right>
+      <MDBNavItem>
+      <MDBDropdown>
+        <MDBDropdownToggle nav caret>
+          <MDBIcon icon="user" />
+        </MDBDropdownToggle>
+        <MDBDropdownMenu className="dropdown-default">
+          <MDBDropdownItem href={ROUTES.SIGN_UP}>Register</MDBDropdownItem>
+          <MDBDropdownItem href={ROUTES.SIGN_IN}>Login</MDBDropdownItem>
+        </MDBDropdownMenu>
+      </MDBDropdown>
+    </MDBNavItem>
+    </MDBNavbarNav>
+    </MDBCollapse>
+    </MDBNavbar>
+    
+    )
+  }
+}
 
 export default Navigation;
