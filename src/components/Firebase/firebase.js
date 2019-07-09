ce09bf71081e23767ecd4662eeecfba47950bb0b
Pratibha Hotwani <pratibhahotwani.glsica15@gmail.com>
2019-07-10 01:02:01 +0530

    Update firebase.js

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -66,7 +66,7 @@ const config = {
       });
       doSendEmailVerification=()=>
       this.auth.currentUser.sendEmailVerification({
-        url:'localhost:3000'
+        url:'localhost:3000/home'
       })
   
     // *** User API ***
